44d022e0ca649e0ece9daed06c2f92eeb21f9af5
Russell Greene <russellgreene8@gmail.com>
2016-01-16 21:38:30 -0700

    Add checking for the Math extension. The extension fails to work if Math isn't loaded, and it gives a nice error message now

--- DMathHooks.php
+++ DMathHooks.php
@@ -7,6 +7,11 @@ if (!defined('MEDIAWIKI')){
 class DMathParse {
     
     function onParserFirstCallSetup(Parser $parser) {
+    
+        if(!ExtensionRegistry::getInstance()->isLoaded('Math')) {
+            die("The DMath extension requires the Math extension, please include it.");
+        }
+    
         $parser->setHook('dmath', 'DMathParse::dmathTagHook' );
         
     }
